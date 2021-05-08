df7f5295cda0fb608f4436a358dbaaa261d4709f
Mark W <numedain@gmail.com>
2021-05-08 09:51:14 +0200

    removed some debugging info

--- replaceToc.sh
+++ replaceToc.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
 if [ -z "${retail}" ]; then
-  echo empty retail
   retail=$(grep -oP 'retail:\K\d+' toc-versions.txt)
 fi
 if [ -z "${bc}" ]; then
-  echo empty bc
   bc=$(grep -oP 'bc:\K\d+' toc-versions.txt)
 fi
 if [ -z "${classic}" ]; then
-  echo empty classic
   classic=$(grep -oP 'classic:\K\d+' toc-versions.txt)
 fi
 
