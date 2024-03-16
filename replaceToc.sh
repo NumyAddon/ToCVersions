73d19465413a3730fed593c58258c842c2956724
MarkW <numedain@gmail.com>
2024-03-16 23:26:10 +0100

    Add cata classic support

--- replaceToc.sh
+++ replaceToc.sh
@@ -3,6 +3,9 @@
 if [ -z "${retail}" ]; then
   retail=100205
 fi
+if [ -z "${cata}" ]; then
+  cata=40400
+fi
 if [ -z "${wrath}" ]; then
   wrath=30403
 fi
@@ -16,7 +19,7 @@ fi
 
 if [ $# -eq 0 ]; then
   find . \( ! -regex '.*/\..*' \) -type f -name "*.toc" -exec sed -i \
-    "s/@toc-version-retail@/$retail/g; s/@toc-version-wrath@/$wrath/g; s/@toc-version-bc@/$bcc/g; s/@toc-version-bcc@/$bcc/g; s/@toc-version-classic@/$classic/g" \
+    "s/@toc-version-retail@/$retail/g; s/@toc-version-cata@/$cata/g; s/@toc-version-wrath@/$wrath/g; s/@toc-version-bc@/$bcc/g; s/@toc-version-bcc@/$bcc/g; s/@toc-version-vanilla@/$classic/g; s/@toc-version-classic@/$classic/g" \
     {} +
 fi
 
@@ -27,4 +30,5 @@ do
   sed -i "s/@toc-version-bc@/$bcc/g" "$var"
   sed -i "s/@toc-version-bcc@/$bcc/g" "$var"
   sed -i "s/@toc-version-classic@/$classic/g" "$var"
+  sed -i "s/@toc-version-vanilla@/$classic/g" "$var"
 done
