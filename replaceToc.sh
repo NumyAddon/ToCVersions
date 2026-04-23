81e621d7cbf9dea8f1fecb6d7cd02d728baa7e34
Mark W <numedain@gmail.com>
2026-04-23 10:54:33 +0200

    Update for retail

--- replaceToc.sh
+++ replaceToc.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 if [ -z "${midnight}" ]; then
-  midnight="120001,120005"
+  midnight=120005
 fi
 if [ -z "${retail}" ]; then
-  retail="120001,120005"
+  retail=120005
 fi
 if [ -z "${mists}" ]; then
   mists=50503
